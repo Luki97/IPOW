369b74c8d86ae440c2e2facf4a090f3bc69478f8
Mateusz Rokosa <mtrokosa97@gmail.com>
2020-10-28 13:14:08 +0100

    interface prototype

--- AppInterface/MainWindow.xaml.cs
+++ AppInterface/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,34 @@ namespace AppInterface
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string FILE_FILTER = "C# file (*.cs)|*.cs";
+        private String fileContent;
+        private String outputPath;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void input_path_btn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = FILE_FILTER;
+            if (openFileDialog.ShowDialog() == true)
+                fileContent = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void output_path_btn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = FILE_FILTER;
+            if (saveFileDialog.ShowDialog() == true)
+                outputPath = saveFileDialog.FileName;
+        }
+
+        private void start_btn_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(outputPath, fileContent);
         }
     }
 }
