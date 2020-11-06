5d8b76e292176599ea64a70bb21ead38c4d25706
Mateusz Rokosa <mtrokosa97@gmail.com>
2020-11-06 21:40:01 +0100

    numeric types change implemented

---
+++ AppInterface/Algorithms/ObfuscationManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AppInterface.Algorithms
+{
+    class ObfuscationManager
+    {
+
+        private CompilationUnitSyntax root;
+
+        public ObfuscationManager(String sourceCode)
+        {
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+            this.root = syntaxTree.GetCompilationUnitRoot();
+        }
+
+        public void ChangeNumericTypes()
+        {
+            NumericTypesRewrite rewriter = new NumericTypesRewrite();
+            root = (CompilationUnitSyntax)rewriter.Visit(root);
+        }
+
+        public String GetSourceCode()
+        {
+            return root.ToFullString();
+        }
+
+    }
+}
